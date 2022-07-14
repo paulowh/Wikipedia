f70644f9f0ef8b31573c808a7d458158fe9a3969
Paulo <ph744915@gmail.com>
2022-07-14 09:38:33 -0300

    Finalização das pesquisa

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import wikipedia
+import time
 
-wikipedia.set_lang("pt")
-#print(wikipedia.summary('Doctor Who'))
+def salvarArquivo(pesq, text):
+    arq = open('pesquisa-'+pesq+'.txt', 'w', encoding="utf-8")
+    arq.writelines(text)
+    arq.close()
 
-wiki = wikipedia.page('Décimo primeiro Doutor')
+def wiki():
+    wikipedia.set_lang("pt")
+    #print(wikipedia.summary('Doctor Who'))
 
-text = wiki.content
-print(text)
+    print(f'Bem vindo ao sistema de pesquisa TESEU')
+    pesq = input('Digite qual a sua pesquisa: ')
+    
+    wiki = wikipedia.page(pesq)
+    
+    print('pesquisando', end='')
+    for i in '...':
+        time.sleep(1)
+        print(i, end='')
+    print('\n')
+    text = wiki.content
+    salvarArquivo(pesq, text)
+    print('Salvo Com Sucesso! \n')
+
+    #print(text)
+
+while True:
+    wiki()
+
+    n = input('deseja uma nova pesquisa? (S or N) ')
+    if n in 'sS':
+        break
